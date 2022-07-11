e4952d85301655f97b470752f26ea82010aca50b
MarcusMix <tradedomarcus@gmail.com>
2022-07-11 16:17:46 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import Tasks from "./components/Tasks";
+
+import './App.css';
+const App = () => {
+    // let message = "hello world";
+    const [tarefas, setTarefas] = useState([
+        {
+            id: '1',
+            title: 'Estudar Programação',
+            completed: false,
+        },
+        {
+            id: '2',
+            title: 'Estudar Inglês',
+            completed: true,
+        }
+    ])
+
+    return (
+    <div>
+        <div className="container">
+        <Tasks />
+        </div>
+       
+    </div>
+ )};
+
+export default App;
+
+
